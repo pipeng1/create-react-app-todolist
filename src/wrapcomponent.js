f5e73ca09c85f59d8cb7354501e104daea7debca
Pipeng1 <549702813@qq.com>
2020-05-13 15:16:08 +0800

    测试使用ssh提交

--- src/wrapcomponent.js
+++ src/wrapcomponent.js
@@ -7,6 +7,7 @@ export default class Home extends React.Component {
             <div>
                 <a href='#/'> 回到todo </a>
                 <div>行有不得，反求诸己</div>
+                <div>测试使用ssh提交</div>
             </div>
         )
     }
